aea1edea33f086e81b1d71169b0bfee810c17a2f
Markus Prettner <markus.prettner@outlook.com>
2024-07-26 23:04:59 +0200

    Add asset files to cmake target

---
+++ Assets/pathtracing_asset_files.cmake
@@ -0,0 +1,15 @@
+
+set(FILES
+    Passes/PathTracingPass.pass
+    Passes/PathTracingPassTemplates.azasset
+    Shaders/PathTracingClosestHit.azsl
+    Shaders/PathTracingClosestHit.shader
+    Shaders/PathTracingClosestHitProcedural.azsl
+    Shaders/PathTracingClosestHitProcedural.shader
+    Shaders/PathTracingCommon.azsli
+    Shaders/PathTracingMiss.azsl
+    Shaders/PathTracingMiss.shader
+    Shaders/PathTracingRayGeneration.azsl
+    Shaders/PathTracingRayGeneration.shader
+    Shaders/Random.azsli
+)
